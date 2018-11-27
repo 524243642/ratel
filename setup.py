4e3d2722999ddb110eaea087d3b04243be76114b
zhangbin <524243642@qq.com>
2018-11-27 16:04:36 +0800

    some problem optimize

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
