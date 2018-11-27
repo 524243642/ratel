f7bcf319decec7aeed72e75dde9d6ce511d97195
zhangbin <524243642@qq.com>
2018-11-27 19:19:10 +0800

    some test added

--- tests/zskiplist/test_zset_obj.py
+++ tests/zskiplist/test_zset_obj.py
@@ -1,5 +1,8 @@
 import unittest
 
+from numpy import NaN
+
+from boost_collections.zskiplist.exception import NotSupportException
 from boost_collections.zskiplist.zset_node import ZsetNode
 from boost_collections.zskiplist.zset_obj import ZsetObj
 
@@ -15,22 +18,33 @@ class TestZsetObj(unittest.TestCase):
         zset_obj = self.zset_obj
         zset_obj.zadd(elements=ZsetNode('d', 1))
         self.assertEqual(4, zset_obj.zcard())
-        zset_obj.zadd(ZsetNode('d', 1), 'nx')
+        zset_obj.zadd(ZsetNode('d', 1), 'nx', None, 'ignore')
         self.assertEqual(4, zset_obj.zcard())
         zset_obj.zadd(ZsetNode('e', 2), 'nx')
         self.assertEqual(5, zset_obj.zcard())
+
+        self.assertRaises(AssertionError, zset_obj.zadd, ZsetNode('e', NaN))
 
     def test_zrem(self):
         zset_obj = self.zset_obj
         zset_obj.zrem('a')
         zset_obj.zrem('b')
         self.assertEqual(1, zset_obj.zcard())
+        deleted = zset_obj.zrem('d')
+        self.assertEqual(0, deleted)
 
     def test_zincrby(self):
         zset_obj = self.zset_obj
         zset_obj.zincrby(ZsetNode('a', 1), 'incr')
         score = zset_obj.zscore('a')
         self.assertEqual(2, score)
+
+        self.assertRaises(NotSupportException, self.zincrby_notsupported_exception)
+
+    def zincrby_notsupported_exception(self):
+        zset_obj = self.zset_obj
+        elements = [ZsetNode('a', 1), ZsetNode('b', 1)]
+        zset_obj.zincrby(elements, 'incr')
 
     def test_zcard(self):
         zset_obj = self.zset_obj
@@ -40,6 +54,12 @@ class TestZsetObj(unittest.TestCase):
         zset_obj = self.zset_obj
         score = zset_obj.zscore('a')
         self.assertEqual(1, score)
+
+        retval = zset_obj.zscore('d')
+        self.assertEqual(None, retval)
+
+        retval = zset_obj.zscore(None)
+        self.assertEqual(None, retval)
 
     def test_zrange(self):
         zset_obj = self.zset_obj
@@ -51,6 +71,14 @@ class TestZsetObj(unittest.TestCase):
         self.assertEqual('a', rets[0].ele)
         self.assertEqual('c', rets[1].ele)
         self.assertEqual('b', rets[2].ele)
+
+        rets = zset_obj.zrange(-5, 5, True)
+        self.assertEqual(3, len(rets))
+
+        rets = zset_obj.zrange(5, -5, True)
+        self.assertEqual(None, rets)
+        rets = zset_obj.zrange(1, 1, True)
+        self.assertEqual('c', rets[0].ele)
 
     def test_zrevrange(self):
         zset_obj = self.zset_obj
